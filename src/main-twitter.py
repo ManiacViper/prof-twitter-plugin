a86544a7c2a19ba3311297fd11b6dea3088e3e9c
ManiacViper <maniacviper@gmail.com>
2014-11-30 19:32:14 +0000

    added a birdy api exception handler

--- src/main-twitter.py
+++ src/main-twitter.py
@@ -1,19 +1,18 @@
 from lib.twitter import UserClient
+from lib.twitter import TwitterClientError
 
 CONSUMER_KEY = '8JakUqhG5yA812RmumwEs0lpx'
 CONSUMER_SECRET = 'ca3P7B8gMiSvjPwD9DcQcgbDB19kIG8Sn416EZ2WeYsnGn6iaE'
-client = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 
-def print_initial_message():
-    global client
-    if client != '':
+def _print_initial_message(client, token):
+    if client and token:
         print('Birdy api version: ' + client.api_version)
         print('Author: ManiacViper')
+        print('Please authenticate profanity here: ' + token['auth_url'])
 
 def authorize_app_for_twitter():
-    global client
     global ACCESS_TOKEN
     global ACCESS_TOKEN_SECRET
     global CONSUMER_KEY
@@ -21,14 +20,12 @@ def authorize_app_for_twitter():
     try:
         client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
         token = client.get_authorize_token()
-    except:
+    except TwitterClientError:
         print('Oops, this is embarrassing, cannot connect to twitter')
     else:
         ACCESS_TOKEN = token['oauth_token']
         ACCESS_TOKEN_SECRET = token['oauth_token_secret']
-        print('Please authenticate profanity here: ' + token['auth_url'])
+        _print_initial_message(client, token)
 
+# execute the program
 authorize_app_for_twitter()
-print_initial_message()
-
-
