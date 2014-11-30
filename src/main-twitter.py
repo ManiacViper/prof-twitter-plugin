b1bb6cb93067d53bf48109269653cb799b1c8445
ManiacViper <maniacviper@gmail.com>
2014-11-30 16:43:54 +0000

    created client with consumer key and secret

--- src/main-twitter.py
+++ src/main-twitter.py
@@ -2,14 +2,8 @@ from lib.twitter import UserClient
 
 CONSUMER_KEY = '8JakUqhG5yA812RmumwEs0lpx'
 CONSUMER_SECRET = 'ca3P7B8gMiSvjPwD9DcQcgbDB19kIG8Sn416EZ2WeYsnGn6iaE'
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+
+client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
 
 
-client = UserClient(CONSUMER_KEY,
-                    CONSUMER_SECRET,
-                    ACCESS_TOKEN,
-                    ACCESS_TOKEN_SECRET)
 
-
-print("hello")
