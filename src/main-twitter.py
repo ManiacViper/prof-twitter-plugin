6078483c2073c680e052acb49eb67cf51c1cf8c3
ManiacViper <maniacviper@gmail.com>
2014-11-30 19:19:18 +0000

    attempted retrieving client in a try block

--- src/main-twitter.py
+++ src/main-twitter.py
@@ -2,27 +2,33 @@ from lib.twitter import UserClient
 
 CONSUMER_KEY = '8JakUqhG5yA812RmumwEs0lpx'
 CONSUMER_SECRET = 'ca3P7B8gMiSvjPwD9DcQcgbDB19kIG8Sn416EZ2WeYsnGn6iaE'
-client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
+client = ''
 ACCESS_TOKEN = ''
 ACCESS_TOKEN_SECRET = ''
 
 def print_initial_message():
-    print('Author: ManiacViper')
-    print('Birdy api version: ' + client.api_version)
+    global client
+    if client != '':
+        print('Author: ManiacViper')
+        print('Birdy api version: ' + client.api_version)
 
 def authorize_twitter_app():
     global client
     global ACCESS_TOKEN
     global ACCESS_TOKEN_SECRET
+    global CONSUMER_KEY
+    global CONSUMER_SECRET
     try:
+        client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
         token = client.get_authorize_token()
     except:
         print('Oops, this is embarrassing, cannot connect to twitter')
     else:
         ACCESS_TOKEN = token['oauth_token']
         ACCESS_TOKEN_SECRET = token['oauth_token_secret']
+        print('Please authenticate profanity here: ' + token['auth_url'])
 
-print_initial_message()
 authorize_twitter_app()
+print_initial_message()
 
 
