c7c7688fc53b030a8277d7856db3dcd9926f6db8
ManiacViper <maniacviper@gmail.com>
2014-11-30 18:56:53 +0000

    gets the oauth token and secret

--- src/main-twitter.py
+++ src/main-twitter.py
@@ -2,8 +2,27 @@ from lib.twitter import UserClient
 
 CONSUMER_KEY = '8JakUqhG5yA812RmumwEs0lpx'
 CONSUMER_SECRET = 'ca3P7B8gMiSvjPwD9DcQcgbDB19kIG8Sn416EZ2WeYsnGn6iaE'
-
 client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
 
+def print_initial_message():
+    print('Author: ManiacViper')
+    print('Birdy api version: ' + client.api_version)
+
+def authorize_twitter_app():
+    global client
+    global ACCESS_TOKEN
+    global ACCESS_TOKEN_SECRET
+    try:
+        token = client.get_authorize_token()
+    except:
+        print('Oops, this is embarrassing, cannot connect to twitter')
+    else:
+        ACCESS_TOKEN = token['oauth_token']
+        ACCESS_TOKEN_SECRET = token['oauth_token_secret']
+
+print_initial_message()
+authorize_twitter_app()
 
 
