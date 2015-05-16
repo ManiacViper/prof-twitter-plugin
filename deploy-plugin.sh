05b4113dd6e6cda2e127a3e7112ee87354028679
maniacviper <maniacviper@gmail.com>
2015-05-16 22:42:38 +0100

    added removal of pyc files in deployment directory, also added profanity python api method to plugin

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -2,6 +2,7 @@
 
 rm -rf ~/.local/share/profanity/plugins/lib/
 rm -rf ~/.local/share/profanity/plugins/src/main-twitter.py
+rm -rf ~/.local/share/profanity/plugins/src/main-twitter.pyc
 
 cp -r src/ ~/.local/share/profanity/plugins/main-twitter.py
 cp -r lib/ ~/.local/share/profanity/plugins/
--- src/main-twitter.py
+++ src/main-twitter.py
@@ -1,6 +1,7 @@
 from lib.twitter import UserClient
 from lib.twitter import TwitterClientError
 from lib.twitter import TwitterApiError
+import prof
 import os
 import json
 
@@ -22,15 +23,18 @@ def authorize_app_for_twitter():
     global access_token_secret
     global CONSUMER_KEY
     global CONSUMER_SECRET
+    authorized = False
 
     try:
         client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
         token = client.get_authorize_token("oob")
     except TwitterClientError:
-        print('Oops, this is embarrassing, cannot connect to twitter')
+         prof.cons_show('Oops, this is embarrassing, cannot connect to twitter')
     else:
         _check_for_token()
-        tweet()
+        prof.cons_show('Okay, your good to go, come ye, birds of the different feathers, we chirp together')
+        authorized = True
+    return authorized
 
 
 def tweet():
@@ -40,23 +44,23 @@ def tweet():
     try:
         tweetApiResponse = client.api.statuses.update.post(status=str(userTweet))
     except TwitterApiError as error:
-        print("Something went wrong in tweeting that")
-        print("Please see error details below:")
-        print("Status code for twitter api: "+ str(error.status_code) + "\n")
+         prof.cons_show("Something went wrong in tweeting that")
+         prof.cons_show("Please see error details below:")
+         prof.cons_show("Status code for twitter api: "+ str(error.status_code) + "\n")
     else:
-        print("Your tweet '" + userTweet + "' has flown away in the clouds")
+         prof.cons_show("Your tweet '" + userTweet + "' has flown away in the clouds")
 
 def stream():
     try:
         #userFeed = client.userstream.user.get() #streaming api error, Unable to decode JSON response
         userFeed = client.api.statuses.home_timeline.get()
     except TwitterApiError as error:
-        print("Something went wrong in getting your user feed")
-        print("Please see error details below:")
-        print("Status code for twitter api: "+ str(error.status_code) + "\n")
+         prof.cons_show("Something went wrong in getting your user feed")
+         prof.cons_show("Please see error details below:")
+         prof.cons_show("Status code for twitter api: "+ str(error.status_code) + "\n")
     else:
         for eachtweet in userFeed.data:
-            print(eachtweet['text'] + "\n")
+             prof.cons_show(eachtweet['text'] + "\n")
 
 # used only by this script
 def _check_for_token():
@@ -78,7 +82,7 @@ def _get_token_from_storage():
     try:
         file_object = open(TOKEN_FILE_NAME, 'r')
     except:
-        print(TOKEN_FILE_NAME + ' file not found')
+         prof.cons_show(TOKEN_FILE_NAME + ' file not found')
     else:
         access_token = file_object.readline().strip()
         access_token_secret = file_object.readline().strip()
@@ -91,10 +95,10 @@ def _print_initial_message():
     global token
 
     if client and token:
-        print('Birdy Twitter API Version: ' + client.api_version)
-        print('Author: ManiacViper')
-        print('Please click below to give your blessings to profanity:')
-        print(token['auth_url'])
+         prof.cons_show('Birdy Twitter API Version: ' + client.api_version)
+         prof.cons_show('Author: ManiacViper')
+         prof.cons_show('Please click below to give your blessings to profanity:')
+         prof.cons_show(token['auth_url'])
 
 def _set_final_access_token():
     global client
@@ -109,7 +113,7 @@ def _set_final_access_token():
                                 access_token, access_token_secret)
             token = client.get_access_token(OAUTH_VERIFIER)
         except TwitterApiError as e:
-            print("getting final access token error: " + e.error_code)
+             prof.cons_show("Getting final access token error: " + e.error_code)
         else:
             access_token = token['oauth_token']
             access_token_secret = token['oauth_token_secret']
@@ -126,7 +130,7 @@ def _save_token():
     try:
         file_object = open(TOKEN_FILE_NAME, 'w')
     except:
-        print('Cannot create a new access token file for twitter')
+         prof.cons_show('Cannot create a new access token file for twitter')
     else:
         file_object.write(access_token + '\n')
         file_object.write(access_token_secret)
@@ -148,7 +152,18 @@ def _quit_application():
     if user_entry != quit_keyword:
         _quit_application()
 
-# execute the program
-authorize_app_for_twitter()
-_quit_application()
+#register profanity commands
+def prof_init(version, status):
+    if authorize_app_for_twitter():
+        #prof.register_timed(_get_scores, 60) - for tweet feed
+        prof.register_command("/tweet", 0, 0,
+            "/tweet",
+            "Chirp what your thinking!",
+            "Chirp what your thinking!",
+            tweet)
+
+#on home screen
+def prof_on_start():
+    if authorize_app_for_twitter():
+        prof.cons_show('you can use /tweet <your tweet> to tweet right now! Do it!')
 
