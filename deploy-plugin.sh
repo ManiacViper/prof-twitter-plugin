bcb11911f49da49cdd501faa3c216f52dfd96176
maniacviper <maniacviper@gmail.com>
2015-05-19 20:36:56 +0100

    deployment script fixed, copying py files correctly from src and lib

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -2,12 +2,12 @@
 
 echo Removing plugin deployment in profanity plugins folder
 rm -rf ~/.local/share/profanity/plugins/lib/
-rm -rf ~/.local/share/profanity/plugins/src/main-twitter.py
-rm -rf ~/.local/share/profanity/plugins/src/main-twitter.pyc
+rm -rf ~/.local/share/profanity/plugins/src/*.py
+rm -rf ~/.local/share/profanity/plugins/src/*.pyc
 ls  ~/.local/share/profanity/plugins/
 
 echo Copying plugin to profanity plugins folder
-cp -r src/ ~/.local/share/profanity/plugins/main-twitter.py
-cp -r lib/ ~/.local/share/profanity/plugins/
+cp -r src/*.py ~/.local/share/profanity/plugins/
+cp -r lib/*.py ~/.local/share/profanity/plugins/
 ls  ~/.local/share/profanity/plugins/
 
