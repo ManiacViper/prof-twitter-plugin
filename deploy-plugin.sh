8d8ac6f9e228b9890ce8ef230f132237c3e0fb73
maniacviper <maniacviper@gmail.com>
2015-06-17 21:35:52 +0100

    deploy script creates a lib directory in prof plugins directory now

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -2,12 +2,13 @@
 
 echo Removing plugin deployment in profanity plugins folder
 rm -rf ~/.local/share/profanity/plugins/lib/
-rm -rf ~/.local/share/profanity/plugins/src/*.py
-rm -rf ~/.local/share/profanity/plugins/src/*.pyc
+rm -rf ~/.local/share/profanity/plugins/*.py
+rm -rf ~/.local/share/profanity/plugins/*.pyc
 ls  ~/.local/share/profanity/plugins/
 
 echo Copying plugin to profanity plugins folder
+mkdir -p ~/.local/share/profanity/plugins/lib
+cp -r lib/*.py ~/.local/share/profanity/plugins/lib/
 cp -r src/*.py ~/.local/share/profanity/plugins/
-cp -r lib/*.py ~/.local/share/profanity/plugins/
-ls  ~/.local/share/profanity/plugins/
+ls -R ~/.local/share/profanity/plugins/
 
